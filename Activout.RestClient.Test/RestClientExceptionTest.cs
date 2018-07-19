2994695c97e1635a140ee417099936a4ccf9b381
David Eriksson <david@activout.se>
2018-07-19 16:29:04 +0200

    Try to make exception properly serializable
    https://blogs.msdn.microsoft.com/agileer/2013/05/17/the-correct-way-to-code-a-custom-exception-class/

---
+++ Activout.RestClient.Test/RestClientExceptionTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using Xunit;
+
+namespace Activout.RestClient.Test
+{
+    // https://blogs.msdn.microsoft.com/agileer/2013/05/17/the-correct-way-to-code-a-custom-exception-class/
+    public class RestClientExceptionTest
+    {
+        [Fact]
+        public void RestClientException_default_ctor()
+        {
+            // Arrange
+            const string expectedMessage = "Exception of type 'Activout.RestClient.RestClientException' was thrown.";
+
+            // Act
+            var sut = new RestClientException(HttpStatusCode.InternalServerError, null);
+
+            // Assert
+            Assert.Null(sut.ErrorResponse);
+            Assert.Null(sut.InnerException);
+            Assert.Equal(HttpStatusCode.InternalServerError, sut.StatusCode);
+            Assert.Equal(expectedMessage, sut.Message);
+        }
+
+        [Fact]
+        public void RestClientException_ctor_string()
+        {
+            // Arrange
+            const string expectedMessage = "message";
+
+            // Act
+            var sut = new RestClientException(HttpStatusCode.InternalServerError, expectedMessage);
+
+            // Assert
+            Assert.Equal(expectedMessage, sut.ErrorResponse);
+            Assert.Null(sut.InnerException);
+            Assert.Equal(HttpStatusCode.InternalServerError, sut.StatusCode);
+            Assert.Equal(expectedMessage, sut.Message);
+        }
+
+        [Fact]
+        public void RestClientException_ctor_string_ex()
+        {
+            // Arrange
+            const string expectedMessage = "message";
+            var innerEx = new Exception("foo");
+
+            // Act
+            var sut = new RestClientException(HttpStatusCode.InternalServerError, expectedMessage, innerEx);
+
+            // Assert
+            Assert.Equal(expectedMessage, sut.ErrorResponse);
+            Assert.Equal(HttpStatusCode.InternalServerError, sut.StatusCode);
+            Assert.Equal(innerEx, sut.InnerException);
+            Assert.Equal(expectedMessage, sut.Message);
+        }
+
+        [Fact]
+        public void RestClientException_serialization_deserialization_test()
+        {
+            // Arrange
+            var innerEx = new Exception("foo");
+            var originalException =
+                new RestClientException(HttpStatusCode.InternalServerError, "message", innerEx);
+            var buffer = new byte[4096];
+            var ms = new MemoryStream(buffer);
+            var ms2 = new MemoryStream(buffer);
+            var formatter = new BinaryFormatter();
+
+            // Act
+            formatter.Serialize(ms, originalException);
+            var deserializedException = (RestClientException) formatter.Deserialize(ms2);
+
+            // Assert
+            Assert.Equal(originalException.StatusCode, deserializedException.StatusCode);
+            Assert.Equal(originalException.InnerException.Message, deserializedException.InnerException.Message);
+            Assert.Equal(originalException.Message, deserializedException.Message);
+            Assert.Equal(originalException.ErrorResponse, deserializedException.ErrorResponse);
+        }
+    }
+}
--- Activout.RestClient/RestClientException.cs
+++ Activout.RestClient/RestClientException.cs
@@ -1,8 +1,28 @@
 ﻿using System;
 using System.Net;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+using Activout.RestClient.Helpers;
 
 namespace Activout.RestClient
 {
+    internal static class Extensions
+    {
+        public static T GetValue<T>(this SerializationInfo self, string name)
+        {
+            return (T) self.GetValue(name, typeof(T));
+        }
+
+        public static bool IsSerializable(this object obj)
+        {
+            if (obj == null)
+                return false;
+
+            var t = obj.GetType();
+            return t.IsSerializable;
+        }
+    }
+
     [Serializable]
     public class RestClientException : Exception
     {
@@ -19,6 +39,17 @@ namespace Activout.RestClient
             ErrorResponse = errorResponse;
         }
 
+        protected RestClientException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            StatusCode = info.GetValue<HttpStatusCode>(nameof(StatusCode));
+            var typeName = info.GetString(nameof(Type));
+            if (typeName != null)
+            {
+                ErrorResponse = info.GetValue(nameof(ErrorResponse), Type.GetType(typeName));
+            }
+        }
+
         public HttpStatusCode StatusCode { get; }
 
         public object ErrorResponse { get; }
@@ -26,6 +57,25 @@ namespace Activout.RestClient
         public T GetErrorResponse<T>()
         {
             return (T) ErrorResponse;
+        }
+
+        [SecurityPermission(SecurityAction.Demand, SerializationFormatter = true)]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            Preconditions.CheckNotNull(info);
+
+            info.AddValue(nameof(StatusCode), StatusCode);
+            if (ErrorResponse.IsSerializable())
+            {
+                info.AddValue(nameof(Type), ErrorResponse.GetType().AssemblyQualifiedName);
+                info.AddValue(nameof(ErrorResponse), ErrorResponse);
+            }
+            else
+            {
+                info.AddValue(nameof(Type), null);
+            }
+
+            base.GetObjectData(info, context);
         }
 
         public override string ToString()
