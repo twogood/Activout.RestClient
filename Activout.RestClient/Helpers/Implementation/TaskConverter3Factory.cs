64dc915d98b5b655e1b457d64100d41460015114
David Eriksson <david@activout.se>
2024-11-25 20:20:02 +0100

    TaskConverter generation 3
    Solves #37

---
+++ Activout.RestClient/Helpers/Implementation/TaskConverter3Factory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Activout.RestClient.Helpers.Implementation;
+
+public class TaskConverter3Factory : ITaskConverterFactory
+{
+    public ITaskConverter CreateTaskConverter(Type actualReturnType)
+    {
+        return actualReturnType == typeof(void) ? null : 
+            (ITaskConverter) Activator.CreateInstance(typeof(TaskConverter3<>).MakeGenericType(actualReturnType));
+    }
+}
