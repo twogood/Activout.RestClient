85e06b11148eca1b04a4e92dc93cc8460b87fea7
David Eriksson <david@activout.se>
2026-02-26 16:03:00 +0100

    Add back typed Part

--- Activout.RestClient/Part.cs
+++ Activout.RestClient/Part.cs
@@ -1,3 +1,18 @@
 namespace Activout.RestClient;
 
-public record Part(object Content, string? Name = null, string? FileName = null);
+public record Part(
+    object? Content,
+    string? Name = null,
+    string? FileName = null);
+
+public record Part<T>(
+    T Content,
+    string? Name = null,
+    string? FileName = null) : Part(Content, Name, FileName)
+{
+    public new T Content
+    {
+        get => (T)base.Content!;
+        init => base.Content = value;
+    }
+}
